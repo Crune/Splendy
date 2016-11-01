3d35922fe0b59fbf2abe45739872f0332d329b36
윤진규 <doov1@naver.com>
2016-11-01 21:22:25 +0900

    @

--- src/main/java/org/kh/splendy/WebConfig.java
+++ src/main/java/org/kh/splendy/WebConfig.java
@@ -1,11 +1,17 @@
 package org.kh.splendy;
 
+import org.kh.splendy.sample.chat.MyHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -16,5 +22,20 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 		super.addResourceHandlers(registry);
 	}
-
+	
+	/**
+	 * 
+	 */
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(myHandler(), "/myHandler").addInterceptors(new HttpSessionHandshakeInterceptor());
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	@Bean
+	public WebSocketHandler myHandler() {
+		return new MyHandler();
+	}
 }
--- src/main/java/org/kh/splendy/sample/chat/SessionMapper.java
+++ src/main/java/org/kh/splendy/sample/chat/SessionMapper.java
@@ -3,32 +3,52 @@ package org.kh.splendy.sample.chat;
 import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
-
+/**
+ * 
+ * @author 진규
+ *
+ */
 public class SessionMapper {
-    private static final ConcurrentHashMap<String, WebSocketSession> websocketSessionMap = new ConcurrentHashMap<String, WebSocketSession>();
-    
-    public static synchronized void add(WebSocketSession session) {
-        websocketSessionMap.put(session.getId(), session);
-    }
-    
-    public static synchronized void remove(WebSocketSession session) {
-        websocketSessionMap.remove(session.getId());
-    }
-    
-    public static void sendAllForWebsocket(String message) {
-        for(Map.Entry<String, WebSocketSession> em : websocketSessionMap.entrySet()){
-                try {
-                    WebSocketSession session = em.getValue();
-                       if(session.isOpen()){
-                           synchronized (session) {
-                               session.sendMessage(new TextMessage(message));
-                           }
-                       }
-                } catch (IOException e) {
-                }
-        }
-    }	
+	private static final ConcurrentHashMap<String, WebSocketSession> websocketSessionMap = new ConcurrentHashMap<String, WebSocketSession>();
+	/**
+	 * 서버에 접속한 클라이언트의 목록
+	 * @param session 접속한 클라이언트
+	 */
+	public static synchronized void add(WebSocketSession cSession) {
+		websocketSessionMap.put(cSession.getId(), cSession);
+		for(Map.Entry<String, WebSocketSession> em : websocketSessionMap.entrySet()){
+			try {
+				WebSocketSession session = em.getValue();
+					if(session.isOpen()){
+						session.sendMessage(new TextMessage(cSession.getRemoteAddress().getHostName()+"님이 입장하셨습니다."));
+					}
+			} catch (IOException e) {
+			}
+		}
+	}
+	/**
+	 * 접속을 종료한 클라이언트의 정보를 제거
+	 * @param session 접속을 종료한 클라이언트
+	 */
+	public static synchronized void remove(WebSocketSession session) {
+		websocketSessionMap.remove(session.getId());
+	}
+	
+	public static void sendAllForWebsocket(WebSocketSession cSession,String message) {
+		for(Map.Entry<String, WebSocketSession> em : websocketSessionMap.entrySet()){
+				try {
+					WebSocketSession session = em.getValue();
+						if(session.isOpen()){
+							synchronized (session) {
+								if(!session.getRemoteAddress().equals(cSession.getRemoteAddress())){
+									session.sendMessage(new TextMessage(cSession.getRemoteAddress().getHostName()+" : "+message));
+								} else { session.sendMessage(new TextMessage("나 : "+message)); }
+							}
+						}
+				} catch (IOException e) {
+				}
+		}
+	}	
 }
