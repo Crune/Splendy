9bd5c1f69e29a53f32371edadf9c5c6a3b44ee8c
최윤 <rune.choe@gmail.com>
2016-11-01 17:17:30 +0900

    .

--- src/main/java/org/kh/splendy/WebConfig.java
+++ src/main/java/org/kh/splendy/WebConfig.java
@@ -1,8 +1,11 @@
 package org.kh.splendy;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
 	@Override
@@ -10,6 +13,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
 		registry.addResourceHandler("/js/**").addResourceLocations("/js/");
 		registry.addResourceHandler("/img/**").addResourceLocations("/img/");
+		
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 		super.addResourceHandlers(registry);
 	}
