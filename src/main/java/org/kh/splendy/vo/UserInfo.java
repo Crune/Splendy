bbb079542c345b8ae3302dc636202c07d9de1893
최윤 <rune.choe@gmail.com>
2016-11-02 15:52:51 +0900

    .

--- src/main/java/org/kh/splendy/vo/UserInfo.java
+++ src/main/java/org/kh/splendy/vo/UserInfo.java
@@ -2,6 +2,9 @@ package org.kh.splendy.vo;
 
 import java.sql.Date;
 
+import lombok.Data;
+
+@Data
 public class UserInfo {
 
 	private int id;
@@ -14,59 +17,5 @@ public class UserInfo {
 	private int notCredential;
 	private Date reg;
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getNickname() {
-		return nickname;
-	}
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public int getEnabled() {
-		return enabled;
-	}
-	public void setEnabled(int enabled) {
-		this.enabled = enabled;
-	}
-	public int getNotLocked() {
-		return notLocked;
-	}
-	public void setNotLocked(int notLocked) {
-		this.notLocked = notLocked;
-	}
-	public int getNotExpired() {
-		return notExpired;
-	}
-	public void setNotExpired(int notExpired) {
-		this.notExpired = notExpired;
-	}
-	public int getNotCredential() {
-		return notCredential;
-	}
-	public void setNotCredential(int notCredential) {
-		this.notCredential = notCredential;
-	}
-	public Date getReg() {
-		return reg;
-	}
-	public void setReg(Date reg) {
-		this.reg = reg;
-	}
 	
 }
