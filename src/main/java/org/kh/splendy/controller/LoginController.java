16f73cebf3e72f9eff1debb7c351b4917e17ec66
minjung <pow0387@naver.com>
2016-11-02 20:24:31 +0900

    minjung

--- src/main/java/org/kh/splendy/controller/LoginController.java
+++ src/main/java/org/kh/splendy/controller/LoginController.java
@@ -10,7 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 	@RequestMapping("/login/facebook")
-	public String index() {
+	public String facebook() {
 		return "user/facebookLogin";
+	}
+	@RequestMapping("/login/google")
+	public String google() {
+		return "user/googleLogin";
+	}
+	@RequestMapping("/main/google")
+	public String google_login() {
+		return "user/hello";
 	}
 }
