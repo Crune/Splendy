4f3565d5fe5abbbc0c986becbf7fd481875b4c7b
dodo <2ehgus@gmail.com>
2016-11-03 20:19:10 +0900

    dohyun

--- src/main/java/org/kh/splendy/controller/LoginController.java
+++ src/main/java/org/kh/splendy/controller/LoginController.java
@@ -1,5 +1,10 @@
 package org.kh.splendy.controller;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 /**
@@ -9,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class LoginController {
+	
+	String storedState;
+	
 	@RequestMapping("/login/facebook")
 	public String facebook() {
 		return "user/facebookLogin";
@@ -20,5 +28,31 @@ public class LoginController {
 	@RequestMapping("/main/google")
 	public String google_login() {
 		return "user/hello";
+	}
+	@RequestMapping("login/naver")
+	public String naver(HttpServletRequest request){
+		String state = generateState();
+		// 세션 또는 별도의 저장 공간에 상태 토큰을 저장
+		request.setAttribute("state", state);
+		
+		System.out.println(state);
+		
+
+		return "user/naver_login";
+	}
+	
+	@RequestMapping("/login/naver_loginPro")
+	public String naverLoginPro(HttpServletRequest request){
+		String code = request.getParameter("code");
+		String state = request.getParameter("state");
+		request.setAttribute("code", code);
+		request.setAttribute("state", state);
+		return "user/naver_loginPro";
+	}
+
+	public String generateState()
+	{
+	    SecureRandom random = new SecureRandom();
+	    return new BigInteger(130, random).toString(32);
 	}
 }
