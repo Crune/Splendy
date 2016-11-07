00319dd9a870081352f85788a6a65cd169a1f67d
최윤 <rune.choe@gmail.com>
2016-11-07 13:08:34 +0900

    AOP, Interceptor 적용

---
+++ src/main/java/org/kh/splendy/SplendyAdvice.java
@@ -0,0 +1,27 @@
+package org.kh.splendy;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class SplendyAdvice {
+
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(SplendyAdvice.class);
+
+	@Around("execution(* org.kh.splendy.controller.*.*(..))")
+	public Object controllerAOP(ProceedingJoinPoint pjp) throws Throwable {
+		log.info(" » Request: op - "+pjp.getSignature().getDeclaringTypeName()+" / "+pjp.getSignature().getName());
+		log.info(" » Request: args - "+Arrays.toString(pjp.getArgs()));
+		Object rst = pjp.proceed();
+		log.info(" » Request: ed - "+pjp.getSignature().getDeclaringTypeName()+" / "+pjp.getSignature().getName());
+		return rst;
+	}
+}
--- src/main/java/org/kh/splendy/service/UserServiceImpl.java
+++ src/main/java/org/kh/splendy/service/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @EnableTransactionManagement
@@ -39,6 +40,7 @@ public class UserServiceImpl implements UserService {
 		return lastId;
 	}
 
+	@Transactional
 	@Override
 	public UserCore join(UserCore newUser) throws Exception {
 		userMap.disabling(newUser.getEmail());
