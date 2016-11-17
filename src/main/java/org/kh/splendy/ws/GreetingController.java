5026d386be28e92583ed4a942364b47e34300237
최윤 <rune.choe@gmail.com>
2016-11-17 15:08:25 +0900

    .

--- src/main/java/org/kh/splendy/ws/GreetingController.java
+++ src/main/java/org/kh/splendy/ws/GreetingController.java
@@ -1,34 +1,13 @@
 package org.kh.splendy.ws;
 
-import org.kh.splendy.service.StreamServiceImpl;
-import org.kh.splendy.service.StreamServiceImpl.WSMsg;
+import org.kh.splendy.vo.WSMsg;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-import com.google.gson.Gson;
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
-import lombok.Data;
-
 @Controller
 public class GreetingController {
 
-	@Data
-	public static class WSMsg {
-
-		@SerializedName("type")
-		@Expose
-		public String type;
-		@SerializedName("cont")
-		@Expose
-		public Object cont;
-
-		public static WSMsg convert(String source) {
-			return new Gson().fromJson(source, WSMsg.class);
-		}
-	}
 
 	@MessageMapping("/msg") // 요청할 주소
 	@SendTo("/topic/lobby") // 구독자가 연결된 주소
