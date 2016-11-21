5b945fa123b20d458dc09d21be33cc83ace80592
진규 <doov1@naver.com>
2016-11-21 20:56:14 +0900

    @

--- src/main/java/org/kh/splendy/mapper/UserInnerMapper.java
+++ src/main/java/org/kh/splendy/mapper/UserInnerMapper.java
@@ -1,5 +1,7 @@
 package org.kh.splendy.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
