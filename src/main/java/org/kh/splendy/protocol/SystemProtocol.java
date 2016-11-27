3b59a4decdf69faa797ecf3bd4df3d163776efc7
Rune <rune.choe@gmail.com>
2016-11-27 14:51:06 +0900

    Update and rename Admin.java to SystemProtocol.java

---
+++ src/main/java/org/kh/splendy/protocol/SystemProtocol.java
@@ -0,0 +1,23 @@
+package org.kh.splendy.protocol;
+
+import org.kh.splendy.vo.WSMsg;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SystemProtocol {
+	
+	@MessageMapping("/help")	
+	public void help(SimpMessageHeaderAccessor headerAccessor, String msg) {
+		
+	}
+	
+	@PreAuthorize("hasRole('admin')")
+	@SendTo("/notice/everyone")
+	public WSMsg notice(SimpMessageHeaderAccessor headerAccessor, WSMsg message) throws Exception {
+		return new WSMsg("notice."+message.getType(), message.getCont());
+	}
+}
