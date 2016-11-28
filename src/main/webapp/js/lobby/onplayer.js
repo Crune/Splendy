a171bd0a986d0e56b8253db22677717296849400
최윤 <rune.choe@gmail.com>
2016-11-28 16:26:28 +0900

    작업중

--- src/main/webapp/js/lobby/onplayer.js
+++ src/main/webapp/js/lobby/onplayer.js
@@ -6,17 +6,46 @@ var playerNew;
 var playerPriv;
 
 function onPlayer() {
-	playerPriv = stompClient.subscribe('/player/private/'+uid, room_priv);
+	playerPriv = stompClient.subscribe('/player/private/'+uid, player_priv);
     
     playerJoin = stompClient.subscribe('/player/join', player_join);
     playerLeft = stompClient.subscribe('/player/left', player_left);
     
-    room_init();
+    player_init();
+    send('player/prev/'+rid, '');
+}
+
+function player_init() {
+    console.log("Chatting initialized!");
+    $(".chat_msg").detach();
+}
+
+function player_priv(evt) {
+	alert('pl:'+evt);
+    var data = JSON.parse(evt.body);
+    if (data.type == 'prev') {
+	    var pl = data.cont;
+		plLen = pl.length;
+		for (i = 0; i < plLen; i++) {
+			var curPl = pl[i];
+			if (curPl.room == 0) {
+				$(".lobby_players").append(temp_player(curPl));
+			} else {
+				$("#room_"+curPl.room+" .row .room_player").append(temp_player(curPl));
+			}
+		}
+    }
+	
 }
 
 function player_join(evt) {
 	
 }
+
+function player_left(evt) {
+	
+}
+
 function joinRoom(rid, password) {
     var room = new Object();
     room.id = rid;
