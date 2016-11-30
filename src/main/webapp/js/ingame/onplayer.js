83c99d6d434c410335abc8dc2fe2f6644db731ab
도현 <2ehgus@gmail.com>
2016-11-30 21:05:44 +0900

    commit5

--- src/main/webapp/js/ingame/onplayer.js
+++ src/main/webapp/js/ingame/onplayer.js
@@ -11,6 +11,7 @@ var playerMap = newMap();
 var playerList = new Array();
 
 function onPlayer() {
+	
     playerPriv = stompClient.subscribe('/player/private/'+uid, player_priv);
 
     playerJoin = stompClient.subscribe('/player/join/'+rid, player_join);
@@ -27,65 +28,59 @@ function player_init() {
 }
 
 function player_refresh(){
+	alert('refresh');
 	$(".player ul").detach();
 	for(i=0; i<playerList.length; i++){
+		console.log(playerMap.get(playerList[i]));
 		$(".player"+(i+1)).html(temp_player(playerMap.get(playerList[i])));
 	}
-	
 }
 
 
 function player_priv(evt) {
+	alert('priv');
     var data = JSON.parse(evt.body);
-    
     if (data.type == 'prev') {
         var pl = data.cont;
-        console.log(pl);
         plLen = pl.length;
         
         for (i = 0; i < plLen; i++) {
         	var curPl = pl[i];
         	playerList.push(curPl.uid);
-        	playerMap.put(curPl.uid, curPl);        	
+        	playerMap.put(curPl.uid, curPl);  
+        	console.log(playerList);
         }
         player_refresh();
     }
 }
 
 function player_join(evt) {
+	alert('join');
     var pl = JSON.parse(evt.body);      
     for(i = 0; i < playerList.length; i++){
     	if(playerList[i].uid === pl.uid){
     		return;
     	}	
-    }
+    }   
+    
     playerMap.put(pl.uid, pl);
     playerList.push(pl.uid);
-    
 	input_chat(new Chat('시스템', pl.nick+'님이 접속하였습니다.','','sys'));
 	player_refresh();
 }
 
 function player_left(evt) {
+	alert('left');
 	var tempArr = new Array();
     var pl = JSON.parse(evt.body);
     
     for(i = 0; i < playerList.length; i++){
-    	if( (pl.uid != playerList[i].uid) && (pl.room != 0) ){
-    		console.log(playerList[i]);
-    		tempArr.push(playerList[i]);
-    		alert('t');
+    	if( pl.room != 0 ){
+    		tempArr.push(playerList[i]);    		
     	}
     }
     playerList = tempArr;
-    $(".player ul").detach();
-    console.log(playerList);
     player_refresh();
-    
-    
 
     input_chat(new Chat('시스템', pl.nick+'님이 나가셨습니다.','','sys'));
-	/* 적절한 위치에 적절하게 붙일것
-	 $("#room_"+pl.room+" .row .room_player").append(temp_player(pl));
-	 */
 }
