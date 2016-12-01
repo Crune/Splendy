8239d498aa95fbdbc024937a2f08caed60e60639
최윤 <rune.choe@gmail.com>
2016-12-01 17:37:41 +0900

    실수 수정

--- src/main/java/org/kh/splendy/controller/IndexController.java
+++ src/main/java/org/kh/splendy/controller/IndexController.java
@@ -25,7 +25,7 @@ public class IndexController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(HttpSession session) {
-        if (isInitialized) { compServ.initialize(); isInitialized = true; }
+        if (!isInitialized) { compServ.initialize(); isInitialized = true; }
         if (session.getAttribute("user") != null) {
             return "redirect:/lobby/";
         } else {
@@ -34,7 +34,7 @@ public class IndexController {
     }
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public String indexWithMsg(@RequestParam("msg") String msg, Model model, HttpSession session) {
-        if (isInitialized) { compServ.initialize(); isInitialized = true; }
+        if (!isInitialized) { compServ.initialize(); isInitialized = true; }
         if (session.getAttribute("user") != null) {
             return "redirect:/lobby/";
         } else {
